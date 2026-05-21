90e6be717a9406c45902057bae82a2ac4a9cbf49
infinty878 <n.kabezhan24@apec.edu.kz>
2026-05-21 21:59:04 +0500

    students list + detail navigation

--- SchoolApp/AppShell.xaml.cs
+++ SchoolApp/AppShell.xaml.cs
@@ -5,6 +5,9 @@
         public AppShell()
         {
             InitializeComponent();
+
+            Routing.RegisterRoute(nameof(StudentsPage), typeof(StudentsPage));
+            Routing.RegisterRoute(nameof(StudentDetailPage), typeof(StudentDetailPage));
         }
     }
 }
